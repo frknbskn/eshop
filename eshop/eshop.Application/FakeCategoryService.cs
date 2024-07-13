5684e2b96956b63a8c7ba319f75c605a1b163d2d
Furkan BASKAN <frknbskn@gmail.com>
2024-07-14 01:13:11 +0300

    added category menu.

---
+++ eshop/eshop.Application/FakeCategoryService.cs
@@ -0,0 +1,22 @@
+﻿using eshop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop.Application
+{
+    public class FakeCategoryService : ICategoryService
+    {
+        public IEnumerable<Category> GetCategories()
+        {
+            return new List<Category>()
+            {
+                new Category(){Id=1,Name="Kitap"},
+                new Category(){Id=2,Name="Müzik"},
+                new Category(){Id=3,Name="Bilgisayar"}
+            };
+        }
+    }
+}
